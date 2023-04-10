2757657c67260d6e60d8741d771a8d63310a870e
eltel <djterrymitchell@gmail.com>
2023-04-09 21:48:05 -0300

    first commit

--- next.config.js
+++ next.config.js
@@ -3,6 +3,23 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  env: {
+    API_URL: "http://localhost:3000",
+    DB_URI:
+      "mongodb+srv://terryTimeline:terry.TimelineOne000@cluster0.osdxp.mongodb.net/timeline?retryWrites=true&w=majority",
+    NEXTAUTH_SECRET: "terryLovesDuda",
+  },
+  images: {
+    domains: [
+      "dribbble.com",
+      "cdn.dribbble.com",
+      "lh3.googleusercontent.com",
+      "unsplash.com",
+      "images.unsplash.com",
+      "platform-lookaside.fbsbx.com",
+      "res.cloudinary.com",
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
